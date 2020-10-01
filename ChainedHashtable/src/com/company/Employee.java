26ef1e3fdce3899faed28f8703286b93aa778131
Anirudh Rajiv Menon <36445587+axe76@users.noreply.github.com>
2020-10-01 12:30:29 +0530

    Add files via upload

---
+++ ChainedHashtable/src/com/company/Employee.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.util.Objects;
+
+public class Employee {
+    private String name;
+    private String lastName;
+    private int id;
+
+    public Employee(String name, String lastName, int id) {
+        this.name = name;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id &&
+                name.equals(employee.name) &&
+                lastName.equals(employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, lastName, id);
+    }
+}
---
+++ ChainedHashtable/src/com/company/StoredEmployee.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class StoredEmployee {
+    public Employee employee;
+    public String key;
+
+    public StoredEmployee(Employee employee, String key) {
+        this.employee = employee;
+        this.key = key;
+    }
+}
